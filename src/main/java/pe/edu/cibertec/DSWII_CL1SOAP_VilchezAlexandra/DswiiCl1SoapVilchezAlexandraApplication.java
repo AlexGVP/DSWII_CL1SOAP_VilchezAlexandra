4f2c6fd8099912c7612fa44f5080794d6ffda522
AlexGVP <i202122156@cibertec.edu.pe>
2024-06-08 10:01:14 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_VilchezAlexandra/DswiiCl1SoapVilchezAlexandraApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_VilchezAlexandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapVilchezAlexandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapVilchezAlexandraApplication.class, args);
+	}
+
+}
